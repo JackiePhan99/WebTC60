7ac5e450002f2a1d868c37178926f5d0615f01cf
jackie <lazmonbiro@gmail.com>
2023-02-13 23:11:32 +0700

    add

--- HomeWorks/Bai1/4-create-modules-2.js
+++ HomeWorks/Bai1/4-create-modules-2.js
@@ -1,4 +1,4 @@
-const myDateModules = require('./4-create-modules.js');
+const myDateModules = require('./4-create-modules-1.js');
 
 const date = myDateModules.myDate.split("-").reverse().join("/");
 console.log(date);
